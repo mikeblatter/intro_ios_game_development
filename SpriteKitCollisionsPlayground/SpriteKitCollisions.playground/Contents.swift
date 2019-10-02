57c880722b014d9c5e55371693f40a3a4341fc65
Michael Blatter <blatter@adobe.com>
2019-10-02 09:07:18 -0700

    add code and updates to slide show

--- SpriteKitCollisionsPlayground/SpriteKitCollisions.playground/Contents.swift
+++ SpriteKitCollisionsPlayground/SpriteKitCollisions.playground/Contents.swift
@@ -2,9 +2,44 @@ import SpriteKit
 import PlaygroundSupport
 
 class Scene: SKScene, SKPhysicsContactDelegate {
+    let blockMargin: CGFloat = 10
+    let blockWidth: CGFloat = 150
+    let blockHeight: CGFloat = 50
+    let rows = 3
+    let columns = 11
+    let numberOfBalls = 2
+    
     struct CategoryBitMask {
         static let Ball: UInt32 = 0b1 << 0
         static let Block: UInt32 = 0b1 << 1
+    }
+    
+    func createBall() -> SKSpriteNode {
+        let ball = SKSpriteNode(color: SKColor.white, size: CGSize(width: 50, height: 50))
+        ball.physicsBody = SKPhysicsBody(rectangleOf: ball.size)
+        ball.physicsBody!.allowsRotation = false
+        ball.physicsBody!.categoryBitMask = CategoryBitMask.Ball
+        ball.physicsBody!.contactTestBitMask = CategoryBitMask.Block | CategoryBitMask.Ball
+        ball.physicsBody!.friction = 0
+        ball.physicsBody!.linearDamping = 0
+        ball.physicsBody!.restitution = 1
+        ball.physicsBody!.velocity = CGVector(dx: 1000, dy: 1000)
+        ball.position = CGPoint(x: 0.5 * super.size.width, y: 0.5 * super.size.height)
+        return ball
+    }
+    
+    func createBlock(x: Int, y: Int) -> SKSpriteNode {
+        let block = SKSpriteNode(color: SKColor.white, size: CGSize(width: blockWidth, height: blockHeight))
+        block.name = "Block"
+        block.physicsBody = SKPhysicsBody(rectangleOf: block.size)
+        block.physicsBody!.categoryBitMask = CategoryBitMask.Block
+        block.physicsBody!.isDynamic = false
+        block.physicsBody!.friction = 0
+        block.physicsBody!.restitution = 1
+        
+        block.position = CGPoint(x: (block.size.width + blockMargin) * CGFloat(x), y: (block.size.height + blockMargin) * CGFloat(y))
+        
+        return block
     }
     
     override func didMove(to view: SKView) {
@@ -19,37 +54,20 @@ class Scene: SKScene, SKPhysicsContactDelegate {
         sceneBound.restitution = 1
         super.physicsBody = sceneBound
         
-        let ball = SKSpriteNode(color: SKColor.white, size: CGSize(width: 50, height: 50))
-        ball.physicsBody = SKPhysicsBody(rectangleOf: ball.size)
-        ball.physicsBody!.allowsRotation = false
-        ball.physicsBody!.categoryBitMask = CategoryBitMask.Ball
-        ball.physicsBody!.contactTestBitMask = CategoryBitMask.Block
-        ball.physicsBody!.friction = 0
-        ball.physicsBody!.linearDamping = 0
-        ball.physicsBody!.restitution = 1
-        ball.physicsBody!.velocity = CGVector(dx: 1000, dy: 1000)
-        ball.position = CGPoint(x: 0.5 * super.size.width, y: 0.5 * super.size.height)
-        super.addChild(ball)
-        
-        let block = SKSpriteNode(color: SKColor.white, size: CGSize(width: 150, height: 50))
-        block.name = "Block"
-        block.physicsBody = SKPhysicsBody(rectangleOf: block.size)
-        block.physicsBody!.categoryBitMask = CategoryBitMask.Block
-        block.physicsBody!.isDynamic = false
-        block.physicsBody!.friction = 0
-        block.physicsBody!.restitution = 1
-        
-        for y in 1...3 {
-            for x in 1...11 {
-                let b = block.copy() as! SKSpriteNode
-                b.position = CGPoint(x: (b.size.width + 10) * CGFloat(x), y: (b.size.height + 10) * CGFloat(y))
-                super.addChild(b)
+        for _ in 1...numberOfBalls {
+            super.addChild(createBall())
+        }
+
+        for y in 1...rows {
+            for x in 1...columns {
+                super.addChild(createBlock(x: x, y: y))
             }
         }
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
-        if contact.bodyA.categoryBitMask == CategoryBitMask.Ball && contact.bodyB.categoryBitMask == CategoryBitMask.Block {
+        if contact.bodyA.categoryBitMask == CategoryBitMask.Ball &&
+            contact.bodyB.categoryBitMask == CategoryBitMask.Block {
             let block = contact.bodyB.node as! SKSpriteNode
             
             if block.name == "Block" {
